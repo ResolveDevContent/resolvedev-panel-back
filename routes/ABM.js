03bc08653d5709064557495214ef21c0afe5f933
LucasMezza <mezza.lucas12@gmail.com>
2024-05-21 21:22:41 -0300

    update

--- routes/ABM.js
+++ routes/ABM.js
@@ -10,23 +10,23 @@ const router = express.Router();
 
 // PRODUCTOS ------------------------------------------------------------------
 
-router.get("/productos/listar", authenticate, (req,res) => { listar(req,res,Producto) } )
-router.get("/productos/listar/:id", authenticate, (req,res) => { listarUno(req,res,Producto) } )
+router.get("/productos/listar", (req,res) => { listar(req,res,Producto) } )
+router.get("/productos/listar/:id", (req,res) => { listarUno(req,res,Producto) } )
 router.post("/productos/agregar", authenticate, (req,res) => { agregar(req,res,Producto) } ) 
 router.put("/productos/modificar/:id", authenticate, (req,res) => { modificar(req,res,Producto) } )
 router.delete("/productos/borrar/:id", authenticate, (req,res) => { borrar(req,res,Producto) } )
 
 // CATEGORIAS -----------------------------------------------------------------
 
-router.get("/categorias/listar", authenticate, (req,res) => { listar(req,res,Categoria) } )
-router.get("/categorias/listar/:id", authenticate, (req,res) => { listarUno(req,res,Categoria) } )
+router.get("/categorias/listar", (req,res) => { listar(req,res,Categoria) } )
+router.get("/categorias/listar/:id", (req,res) => { listarUno(req,res,Categoria) } )
 router.post("/categorias/agregar", authenticate, (req,res) => { agregar(req,res,Categoria) } )
 router.put("/categorias/modificar/:id", authenticate, (req,res) => { modificar(req,res,Categoria) } )
 router.delete("/categorias/borrar/:id", authenticate, (req,res) => { borrar(req,res,Categoria) } )
 
 // TIENDA -----------------------------------------------------------------
 
-router.get("/tienda/listar", authenticate, (req,res) => { listar(req,res,Tienda) } )
+router.get("/tienda/listar", (req,res) => { listar(req,res,Tienda) } )
 router.post("/tienda/agregar", authenticate, (req,res) => { agregar(req,res,Tienda) } )
 router.put("/tienda/modificar/:id", authenticate, (req,res) => { modificar(req,res,Tienda) } )
 
